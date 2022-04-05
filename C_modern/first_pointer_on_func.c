ea2e028caa2ad90d033c64b87fe9fd3f124679bd
Nather <qwftg2004@gmail.com>
2022-04-05 17:33:25 +0300

    pointer+typedef

---
+++ C_modern/first_pointer_on_func.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <math.h>
+#include<time.h>
+#include<stdlib.h>
+
+//
+void get_memory_matrix(int***, int*);
+//
+void generate_matrix(int** , int);
+//
+void print_matrix(int**, int);
+//
+void free_memory(int**, int);
+
+int main()
+{
+    int** pm;
+    int i, j, n;
+    void (*pf)(int** , int); //pointer on func
+    void (*pf_array[])(int** , int) = {generate_matrix, print_matrix, free_memory}; // mas of pointers on funcs
+    
+    srand(time(NULL));
+    
+    get_memory_matrix(&pm, &n);
+    
+    (*pf_array[0])(pm, n);
+
+    (*pf_array[1])(pm, n);
+    
+    (*pf_array[2])(pm, n);
+    
+}
+
+void get_memory_matrix(int*** ppm, int* pn)
+{
+    int i, n;
+    int** pm;
+    
+    n = rand() % 3 + 2;
+    *pn = n;
+    
+    pm = (int**)malloc(n * sizeof(int*)); 
+    *ppm = pm;
+    
+    for(i=0;i<n;i++){
+        pm[i] = (int*)malloc(n * sizeof(int));
+    }
+}
+
+void generate_matrix(int** pm, int n)
+{
+    int i,j;
+    
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+           pm[i][j] = rand() % 10;
+        }
+    }
+}
+
+void print_matrix(int** pm, int n)
+{
+    int i,j;
+    
+    printf("\nPrinting of matrix\n");
+    
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+           printf("%4d|",pm[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void free_memory(int** pm, int n)
+{
+    int i;
+    
+    for(i=0;i<n;i++) 
+       if(pm[i]) free(pm[i]);
+    
+    if (pm) free(pm);
+}
---
+++ C_modern/first_typedef.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <math.h>
+#include<time.h>
+#include<stdlib.h>
+
+//
+typedef float (*pfunc)(float);
+//
+float sin_2(float x);
+
+//
+float cos_2(float x);
+
+//
+void print_table(float, float, float, pfunc);
+
+int main ()
+{
+    float x;
+    float (*pf_array[])(float) = {sin_2 , cos_2};
+    int choise;
+
+    while (1){
+        printf("\npush 1 for sin\n");
+        printf("push 2 for cos\n");
+        printf("push 3 for exit\n");
+        scanf("%d", &choise);
+        if (choise == 1 || choise == 2)  print_table(1, 10, 1, pf_array[choise - 1]);
+        else break;
+    }
+    
+    return 0;
+}
+
+void print_table(float a, float b, float step, pfunc pf)
+{
+    float x = a;
+    
+    for(;x<=b; x+=step){
+        printf("\nx = %5.3f f(x) = %5.3f", x, (*pf)(x));
+    }
+}
+
+float sin_2(float x)
+{
+    return sin(x) * sin(x);
+}
+float cos_2(float x)
+{
+    return cos(x) * cos(x);
+}
+
