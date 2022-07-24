a4b2d0590eeee4663545b4273b702a45d95276cd
Nather <qwftg2004@gmail.com>
2022-07-24 11:26:12 +0300

    askme

---
+++ C_modern/olim.1.4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int n, k;
+
+    scanf("%d %d", &n, &k);
+
+    if(n == k){
+        printf("0");
+        return 0;
+    }
+    for(i = 0, t = 0; i < n; i++){
+        
+        t += 1;
+    }
+}
